789239eb7a4f9fffd9e01940f95c54d951cd244a
benni90100 <micelibenedetto800@gmail.com>
2023-12-21 21:00:53 +0100

    implementata funzione js cambio img

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 //BENNI sezione earn script che modifica l'immagine al passaggio del mouse
+//IMPORTANTE! scaricare le immagini delle card e inserirle dentro la directory ./assets
+//i percorsi degli assets vanno posti dentro gli array in ordine, in quanto la funzione associa
+//le immagini con indici uguali
+const imageHover = document.querySelectorAll(".card-image-earn")
 
-const imageHover = document.querySelector(".card-image")
+const dataImageEarn = [
+    "assets/earn-farm.webp",
+    "assets/earn-fixed-staking.webp",
+    "assets/earn-liquidity-staking.webp",
+    "assets/earn-pools.webp"
 
-imageHover.addEventListener("mouseover", ()=>{
-    imageHover.src = "assets/earn-farm.webp"
-    imageHover.addEventListener("mouseout",()=>{
-        imageHover.src = "assets/earn-farm-purple.webp"
+]
+const dataImageOutEarn = [
+    "assets/earn-farm-purple.webp",
+    "assets/earn-fixed-staking-purple.webp",
+    "assets/earn-liquidity-staking-purple.webp",
+    "assets/earn-pools-purple.webp"
+
+]
+imageHover.forEach((image,index) => {
+    image.addEventListener("mouseover", () => {
+        image.src=dataImageEarn[index]
     })
-})
+    image.addEventListener("mouseout", () => {
+        image.src=dataImageOutEarn[index]
+    })
+
+
+    })
