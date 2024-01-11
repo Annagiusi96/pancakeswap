ef081a40dfc137120455c44f02295f41788768cc
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-01-11 16:23:48 +0100

    first commit annagiusi

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    // const thema = document.querySelector('html')
+    // const btnChangeTheme = document.querySelector('button')
+    // const homeDue = document.querySelector('.home-2')
+    
+    // btnChangeTheme.addEventListener('click',()=>{
+    //     let value = thema.getAttribute('data-theme');
+    //     if(value === 'dark'){
+    //         thema.setAttribute('data-theme','light')
+    //         homeDue.classList.Add('home-2-dark')
+    //     }
+    //     if(value == 'light'){
+    //         thema.setAttribute('data-theme','dark')
+    //         homeDue.classList.Add('home-2-light')
+    //     }
+    // })
