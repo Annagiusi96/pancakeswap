7f107d29624f02527ad6a8340be13e529f4c8317
Annagiusi <148056449+Annagiusi@users.noreply.github.com>
2024-01-12 10:45:52 +0100

    modifi animaz

--- index.js
+++ index.js
@@ -15,40 +15,6 @@
 //     //     }
 //     // })
 
-//     <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Carosello</title>
-//     <style>
-//         /* Aggiungi qui i tuoi stili CSS */
-//         .shaping-infinite-scrolling-container {
-//             overflow: hidden;
-//             white-space: nowrap;
-//         }
-
-//         .shaping-scroll-element {
-//             display: inline-block;
-//             margin-right: 20px; /* Aggiungi spaziatura tra gli elementi del carosello */
-//         }
-//     </style>
-// </head>
-// <body>
-
-// <div class="shaping-infinite-scrolling-container">
-//     <!-- Aggiungi qui i tuoi elementi del carosello -->
-//     <div class="shaping-scroll-element" id="scroll-element-1-background" class="animation">
-//         <!-- Contenuto del primo elemento -->
-//     </div>
-//     <div class="shaping-scroll-element" id="scroll-element-2-background" class="animation">
-//         <!-- Contenuto del secondo elemento -->
-//     </div>
-//     <!-- Aggiungi altri elementi secondo necessità -->
-// </div>
-
-
-   
     const shapingCarosello = document.querySelector('.shaping-infinite-scrolling-container');
     const shapingElements = document.querySelectorAll('.shaping-scroll-element')
    
