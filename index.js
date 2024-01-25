7f171f2c4dde7adda56438814f0ea2bda0f495a4
benni90100 <micelibenedetto800@gmail.com>
2024-01-25 22:42:24 +0100

    monetinaindev

--- index.js
+++ index.js
@@ -142,146 +142,152 @@ const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 }
 
 observer.observe(img);
 //inizio
-const canvasMonetina = document.querySelector('.canvasMonetina');
-const ctx = canvasMonetina.getContext('2d')
-canvasMonetina.width = 800
-canvasMonetina.height = 800
-//linea orizontale s 
-const startX = 300;
-const startY = 500
-const endX = 150
-const endY = 500
-ctx.strokeStyle = 'white';
-ctx.lineWidth = 2;
-ctx.zIndex = 1000
-let currentX = startX
+//selettori dei container dei caroselli
+const shaping1 = document.querySelector(`#show-scroll`)
+const shaping2 = document.querySelector(`.shaping-scroll-element2`)
 
-//metodi doszionedellinee sul canvas
-function sxLineDraw() {
-    currentX -= 2
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(currentX, endY);
-    ctx.stroke();
-    ctx.closePath();
-    if (currentX == (startX - endX)) {
-        clearInterval(intervalIDlineSxOrizontal)
+    const canvasMonetina = document.querySelector('.canvasMonetina');
+    const ctx = canvasMonetina.getContext('2d')
+    canvasMonetina.width = 800
+    canvasMonetina.height = 800
+    //linea orizontale s 
+    const startX = 300;
+    const startY = 500
+    const endX = 150
+    const endY = 500
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+    ctx.zIndex = 1000
+    let currentX = startX
+
+    //metodi doszionedellinee sul canvas
+    function sxLineDraw() {
+        currentX -= 2
+        ctx.beginPath();
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(currentX, endY);
+        ctx.stroke();
+        ctx.closePath();
+        if (currentX == (startX - endX)) {
+            clearInterval(intervalIDlineSxOrizontal)
+        }
     }
-}
-const intervalIDlineSxOrizontal = setInterval(() => { sxLineDraw() }, 10)
-//linea orizontale destra
-const startMenoX = 500;
-const startMenoY = 300
-const endMenoX = 650
-const endMenoY = 300
-ctx.strokeStyle = 'White';
-ctx.lineWidth = 2;
-ctx.zIndex = 1000
-let currentDXX = startMenoX
-let intervalIDlineDxOrizontal
-//metodi di costruzione delle linee sul canvas
-function dxLineDraw() {
-    currentDXX += 2
-    ctx.beginPath();
-    ctx.moveTo(startMenoX, startMenoY);
-    ctx.lineTo(currentDXX, endMenoY);
-    ctx.stroke();
-    ctx.closePath();
-    if (currentDXX === 650) {
-        clearInterval(intervalIDlineDxOrizontal)
+    const intervalIDlineSxOrizontal = setInterval(() => { sxLineDraw() }, 10)
+    //linea orizontale destra
+    const startMenoX = 500;
+    const startMenoY = 300
+    const endMenoX = 650
+    const endMenoY = 300
+    ctx.strokeStyle = 'White';
+    ctx.lineWidth = 2;
+    ctx.zIndex = 1000
+    let currentDXX = startMenoX
+    let intervalIDlineDxOrizontal
+    //metodi di costruzione delle linee sul canvas
+    function dxLineDraw() {
+        currentDXX += 2
+        ctx.beginPath();
+        ctx.moveTo(startMenoX, startMenoY);
+        ctx.lineTo(currentDXX, endMenoY);
+        ctx.stroke();
+        ctx.closePath();
+        if (currentDXX === 650) {
+            clearInterval(intervalIDlineDxOrizontal)
 
+        }
     }
-}
-//parte la seconda linea dopo che ha finito la prima
-setTimeout(() => {
-    intervalIDlineDxOrizontal = setInterval(() => dxLineDraw(), 10)
-}, 1500);
+    //parte la seconda linea dopo che ha finito la prima
+    setTimeout(() => {
+        intervalIDlineDxOrizontal = setInterval(() => dxLineDraw(), 10)
+    }, 1500);
 
 
-//linea verticale  destra
-const startSottoSxX = 150
-const startSottoSxY = 500
-const endSottoSxX = 150
-const endSottoSxY = 700
-ctx.strokeStyle = 'White';
-ctx.lineWidth = 2;
-ctx.zIndex = 1000
-let idIntervallSuLeft
-let idIntervallGiuLeft
-let currentLeftIdgiu = startSottoSxY
-let currentLeftIdSU = startSottoSxY
-//metodi di costruzione delle linee sul canvas
-function verticalLineDrawLeft() {
-    currentLeftIdSU -= 5
-    ctx.beginPath();
-    ctx.moveTo(startSottoSxX, startSottoSxY);
-    ctx.lineTo(endSottoSxX, currentLeftIdSU);
-    ctx.stroke();
-    ctx.closePath();
-    if (currentLeftIdSU === startSottoSxX) {
-        clearInterval(idIntervallSuLeft)
+    //linea verticale  destra
+    const startSottoSxX = 150
+    const startSottoSxY = 500
+    const endSottoSxX = 150
+    const endSottoSxY = 700
+    ctx.strokeStyle = 'White';
+    ctx.lineWidth = 2;
+    ctx.zIndex = 1000
+    let idIntervallSuLeft
+    let idIntervallGiuLeft
+    let currentLeftIdgiu = startSottoSxY
+    let currentLeftIdSU = startSottoSxY
+    //metodi di costruzione delle linee sul canvas
+    function verticalLineDrawLeft() {
+        currentLeftIdSU -= 5
+        ctx.beginPath();
+        ctx.moveTo(startSottoSxX, startSottoSxY);
+        ctx.lineTo(endSottoSxX, currentLeftIdSU);
+        ctx.stroke();
+        ctx.closePath();
+        if (currentLeftIdSU === startSottoSxX) {
+            clearInterval(idIntervallSuLeft)
+        }
     }
-}
-function verticalLineDrawLeftSu() {
-    currentLeftIdgiu += 2
-    console.log(currentLeftIdgiu);
-    ctx.beginPath();
-    ctx.moveTo(startSottoSxX, startSottoSxY);
-    ctx.lineTo(endSottoSxX, currentLeftIdgiu);
-    ctx.stroke();
-    ctx.closePath();
-    if (currentLeftIdgiu === 700) {
-        clearInterval(idIntervallGiuLeft)
+    function verticalLineDrawLeftSu() {
+        currentLeftIdgiu += 2
+        console.log(currentLeftIdgiu);
+        ctx.beginPath();
+        ctx.moveTo(startSottoSxX, startSottoSxY);
+        ctx.lineTo(endSottoSxX, currentLeftIdgiu);
+        ctx.stroke();
+        ctx.closePath();
+        if (currentLeftIdgiu === 700) {
+            clearInterval(idIntervallGiuLeft)
+            shaping1.classList.add(`show-scroll`)
+        }
     }
-}
-setTimeout(() => {
-    idIntervallSuLeft = setInterval(() => verticalLineDrawLeft(), 10)
-    idIntervallGiuLeft = setInterval(() => verticalLineDrawLeftSu(), 10)
-}, 1000);
+    setTimeout(() => {
+        idIntervallSuLeft = setInterval(() => verticalLineDrawLeft(), 10)
+        idIntervallGiuLeft = setInterval(() => verticalLineDrawLeftSu(), 10)
+    }, 1000);
 
 
 
-//linea verticale  sinistra
-const startSottoDxX = 650
-const startSottoDxY = 300
-const endSottoDxX = 650
-const endSottoDxY = 700
-ctx.strokeStyle = 'White'; // Rosso
-ctx.lineWidth = 2; // Spessore della linea
-ctx.zIndex = 1000
-let currentIdVerticalRightUp = startSottoDxY
-//metodi di costruzione delle linee sul canvas
-function verticalLineDrawrightSu() {
-    currentIdVerticalRightUp += 5
-    ctx.beginPath();
-    ctx.moveTo(startSottoDxX, startSottoDxY);
-    ctx.lineTo(endSottoDxX, currentIdVerticalRightUp);
-    ctx.stroke();
-    ctx.closePath();
-    if (currentIdVerticalRightUp == 700) {
-        clearInterval(intervalIDUpRight)
+    //linea verticale  sinistra
+    const startSottoDxX = 650
+    const startSottoDxY = 300
+    const endSottoDxX = 650
+    const endSottoDxY = 700
+    ctx.strokeStyle = 'White'; // Rosso
+    ctx.lineWidth = 2; // Spessore della linea
+    ctx.zIndex = 1000
+    let currentIdVerticalRightUp = startSottoDxY
+    //metodi di costruzione delle linee sul canvas
+    function verticalLineDrawrightSu() {
+        currentIdVerticalRightUp += 5
+        ctx.beginPath();
+        ctx.moveTo(startSottoDxX, startSottoDxY);
+        ctx.lineTo(endSottoDxX, currentIdVerticalRightUp);
+        ctx.stroke();
+        ctx.closePath();
+        if (currentIdVerticalRightUp == 700) {
+            clearInterval(intervalIDUpRight)
+        }
     }
-}
-setTimeout(() => {
-    intervalIDUpRight = setInterval(() => verticalLineDrawrightSu(), 10);
-    intervalIDDownRight = setInterval(() => verticalLineDrawrightGiu(), 10);
-}, 2200);
+    setTimeout(() => {
+        intervalIDUpRight = setInterval(() => verticalLineDrawrightSu(), 10);
+        intervalIDDownRight = setInterval(() => verticalLineDrawrightGiu(), 10);
+    }, 2200);
 
 
-let currentIdVerticalRightDown = startSottoDxY
-let intervalIDDownRight
-//metodi di costruzione delle linee sul canvas
-function verticalLineDrawrightGiu() {
-    currentIdVerticalRightDown -= 2
-    ctx.beginPath();
-    ctx.moveTo(startSottoDxX, startSottoDxY);
-    ctx.lineTo(endSottoDxX, currentIdVerticalRightDown);
-    ctx.stroke();
-    ctx.closePath();
-    if (currentIdVerticalRightDown == 150) {
-        clearInterval(intervalIDDownRight)
+    let currentIdVerticalRightDown = startSottoDxY
+    let intervalIDDownRight
+    //metodi di costruzione delle linee sul canvas
+    function verticalLineDrawrightGiu() {
+        currentIdVerticalRightDown -= 2
+        ctx.beginPath();
+        ctx.moveTo(startSottoDxX, startSottoDxY);
+        ctx.lineTo(endSottoDxX, currentIdVerticalRightDown);
+        ctx.stroke();
+        ctx.closePath();
+        if (currentIdVerticalRightDown == 150) {
+            clearInterval(intervalIDDownRight)
+        }
     }
-}
+
 
 const shapingCarosello1 = document.querySelector('.shaping-infinite-scrolling-container1');
 const shapingElements1 = document.querySelectorAll('.shaping-scroll-element1')
